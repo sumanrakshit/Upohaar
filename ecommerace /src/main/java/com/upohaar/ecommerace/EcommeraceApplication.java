fbfd18555c644fb1b3179aa25329e1da747c8b19
suman rakshit <sumanrakshit0305@gmail.com>
2024-03-12 16:26:34 +0530

    spring initilizer

---
+++ ecommerace /src/main/java/com/upohaar/ecommerace/EcommeraceApplication.java
@@ -0,0 +1,13 @@
+package com.upohaar.ecommerace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommeraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommeraceApplication.class, args);
+	}
+
+}
---
+++ ecommerace /src/test/java/com/upohaar/ecommerace/EcommeraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upohaar.ecommerace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommeraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
