e4a38bab3264cfcfd7a195c1de9bc0f638940ca3
Vivek Saini <viveksaini.java7838@gmail.com>
2024-03-12 18:02:40 +0530

    add controller

--- ecommerace /src/main/java/com/upohaar/ecommerace/entity/Product.java
+++ ecommerace /src/main/java/com/upohaar/ecommerace/entity/Product.java
@@ -5,14 +5,9 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Getter
 @Setter
